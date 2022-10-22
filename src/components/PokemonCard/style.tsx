f3610ed597248a775e1fd8e7f0eb960c9e2f1bb7
Lucas Tadeu Vaz <lucastadeuvaz@gmail.com>
2022-10-22 19:24:28 -0300

    Update style.tsx

--- src/components/PokemonCard/style.tsx
+++ src/components/PokemonCard/style.tsx
@@ -58,9 +58,9 @@ const MaxCp = styled.div`
   border-radius: 4px;
 
   span {
-   display: flex;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     font-size: 14px;
     font-weight: bold;
 
